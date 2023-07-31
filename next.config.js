c5ba6518efeb8da539f83049df9ce49857bcd8c9
Maurício Schiavo <mauricio@atendare.com>
2023-07-30 23:01:39 -0300

    New: references

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    poweredByHeader: false,
+    env: {
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId
+    }
+}
 
 module.exports = nextConfig
